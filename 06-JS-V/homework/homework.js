4635a9dfc5ce8a384af8759b5c423a2610420c26
luciano-plaza <lucianomps2015@gmail.com>
2022-01-13 12:55:24 -0300

    JSV passed

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -83,7 +83,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  const persona = new Persona('Juan', 'Perez', 22, 'Saavedra 123');
+  const persona = new Persona(nombre, apellido, edad, dir);
   return persona;
 }
   
@@ -93,7 +93,6 @@ function agregarMetodo() {
   Persona.prototype.datos = function() {
     return this.nombre + ", " + this.edad + " años";
   }
-  return Persona.datos;
 }
   
 
